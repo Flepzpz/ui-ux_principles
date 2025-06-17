d7360673ec835e7aa7866dfa4d36c5442a5019a5
Felipe Olivato Coimbra <felipe.olicoimbra@gmail.com>
2025-06-17 14:41:58 -0300

    refactor: Removed categories page

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ with open('styles/style.css') as file:
     css = file.read()
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-pg = st.navigation(['Registro_Completo.py', 'Dashboard_Vendas.py', 'Dashboard_Receita.py', 'Dashboard_Produtos.py', 'Dashboard_Categorias.py', 'Dashboard_Clientes.py', 'Lista_de_Produtos.py'])
+pg = st.navigation(['Registro_Completo.py', 'Dashboard_Vendas.py', 'Dashboard_Receita.py', 'Dashboard_Produtos.py', 'Dashboard_Clientes.py', 'Lista_de_Produtos.py'])
 pg.run()
 
